4704a4fcc55f7435920148197927e5074d82dd66
Burak Gulseren <gulserenburak@gmail.com>
2020-11-29 11:59:52 -0700

    Added transaction form.

--- Project/src/Payment/TransactionForm.java
+++ Project/src/Payment/TransactionForm.java
@@ -2,7 +2,26 @@ package Payment;
 
 public class TransactionForm {
 
-    public void creditCardForm(int amount) {
-
+	private String cc;
+	private int cvv;
+	private double amount;
+	private String result;
+	private FinancialInstitute fi;
+	private java.util.Date date;
+	
+    public TransactionForm(String cc, int cvv, double amount) {
+    	this.amount = amount;
+    	this.cvv = cvv;
+    	this.cc = cc;
+    	this.date = new java.util.Date();
+    	this.result = "NOT_SUBMITTED";
     }
+    
+    public void setResult(String result) {
+		this.result = result;
+	}
+    
+    public String getResult() {
+		return result;
+	}
 }
