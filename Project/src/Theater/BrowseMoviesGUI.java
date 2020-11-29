67f6a96a142cb6d5c94e0639b540ef62722af8b4
santiagofloresm <64857391+santiagofloresm@users.noreply.github.com>
2020-11-28 22:09:09 -0700

    Added files from Lotfi

---
+++ Project/src/Theater/BrowseMoviesGUI.java
@@ -0,0 +1,216 @@
+package Theater;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseAdapter;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+
+
+public class BrowseMoviesGUI extends JFrame{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private ManageTheater manageTheater;
+	
+	private JComboBox<ShowTime> showtimesComboBox;
+	private JTable table;
+	private JScrollPane scrollPane;
+	private JButton listAllMoviesButton;
+	private JLabel welcomeLabel;
+	private JLabel searchByShowtimeLabel;
+	private JTextField txtEnterNameHere;
+	private JLabel searchName;
+	private JButton searchButton;
+	private JButton confirmSelectionButton ;
+	private JLabel posterLabel;
+	private String[] headers;
+	private JButton returnHomeButton; //need to add
+	private JLabel lblNewLabel; // need to add
+	
+	
+	public BrowseMoviesGUI(String[][] moviesWithAccess, boolean userStatus, ShowTime[] showtimes) {
+		
+		getContentPane().setBackground(new Color(176, 196, 222));
+		getContentPane().setLayout(null);
+		
+		welcomeLabel = new JLabel("Please have a look at our movies!");
+		welcomeLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		welcomeLabel.setBounds(71, 48, 411, 48);
+		getContentPane().add(welcomeLabel);
+		
+		listAllMoviesButton = new JButton("List All Movies");
+		listAllMoviesButton.setBounds(133, 111, 138, 35);
+		getContentPane().add(listAllMoviesButton);
+		
+		showtimesComboBox = new JComboBox<ShowTime>(showtimes);
+		showtimesComboBox.setBounds(103, 192, 213, 22);
+		getContentPane().add(showtimesComboBox);
+		
+		searchByShowtimeLabel = new JLabel("Search by Showtime:");
+		searchByShowtimeLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		searchByShowtimeLabel.setBounds(133, 157, 133, 35);
+		getContentPane().add(searchByShowtimeLabel);
+		
+		txtEnterNameHere = new JTextField();
+		txtEnterNameHere.setText("Enter Name Here");
+		txtEnterNameHere.setBounds(202, 269, 114, 20);
+		getContentPane().add(txtEnterNameHere);
+		txtEnterNameHere.setColumns(10);	
+		txtEnterNameHere.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				txtEnterNameHere.setText("");
+			}
+		});
+		
+		searchName = new JLabel("Search by Movie Name:");
+		searchName.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		searchName.setBounds(133, 215, 172, 59);
+		getContentPane().add(searchName);
+		
+		searchButton = new JButton("Search");
+		searchButton.setBounds(103, 268, 89, 23);
+		getContentPane().add(searchButton);
+		
+		confirmSelectionButton = new JButton("Confirm Selection");
+		confirmSelectionButton.setBackground(new Color(255, 218, 185));
+		confirmSelectionButton.setBounds(133, 302, 138, 22);
+		getContentPane().add(confirmSelectionButton);
+		
+		posterLabel = new JLabel();
+		posterLabel.setBounds(102, 335, 226, 265);
+		getContentPane().add(posterLabel);
+		
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(457, 11, 492, 589);
+        getContentPane().add(scrollPane);
+        
+        returnHomeButton = new JButton("Return to Home");
+        returnHomeButton.setBounds(10, 8, 135, 35);
+        getContentPane().add(returnHomeButton);
+        
+        lblNewLabel = new JLabel("Login Status: Not Logged In");
+        lblNewLabel.setBackground(new Color(255, 222, 173));
+        lblNewLabel.setBounds(298, 8, 160, 35);
+        getContentPane().add(lblNewLabel);
+        
+        if(userStatus == true) {
+        	lblNewLabel.setText("Login Status: Logged In");
+        }
+        
+        headers = new String[] {"Movie Name", "Early Access"};
+		
+    	setUpTable(moviesWithAccess);
+    	
+		initializeFrame();
+	}
+
+	private void initializeFrame() {
+		this.setTitle("Browse Movies");
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setBounds(100, 140, 1000, 650);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+	
+	
+	public void addConfirmMovieButtonListener (ActionListener listenForConfirmMovieButton) {
+		confirmSelectionButton.addActionListener(listenForConfirmMovieButton);
+	}
+	
+	public void addSearchButtonListener (ActionListener listenForSearchButton) {
+		searchButton.addActionListener(listenForSearchButton);
+	}
+	
+	public void addListAllButtonListener(ActionListener listenForListAllButton) {
+		listAllMoviesButton.addActionListener(listenForListAllButton);
+	}
+	
+	public void addMovieRowListener(MouseAdapter listenForClick) {
+		table.addMouseListener(listenForClick);
+	}
+	
+	public void addReturnListener(ActionListener returnHome) {
+		returnHomeButton.addActionListener(returnHome);
+	}
+	
+	public JComboBox<ShowTime> getShowtimesComboBox() {
+		return showtimesComboBox;
+	}
+
+	public JButton getListAllMoviesButton() {
+		return listAllMoviesButton;
+	}
+
+	public JButton getSearchButton() {
+		return searchButton;
+	}
+	
+	public String getSearchString() {
+		return txtEnterNameHere.getText();
+	}
+
+	public JButton getConfirmSelectionButton() {
+		return confirmSelectionButton;
+	}
+
+	public void addComboBoxListener(ActionListener listenForComboBox) {
+		showtimesComboBox.addActionListener(listenForComboBox);
+	}
+	
+	
+	public String getMovieNameSelection() {
+		int selectedRow = table.getSelectedRow();
+		String movieName = (String) table.getValueAt(selectedRow, 0);
+		return movieName;
+	}
+	
+	
+	private void setUpTable(String [][] moviesWithAccess) {
+		table = new JTable() {
+			private static final long serialVersionUID = 1L;
+
+			public boolean isCellEditable(int row, int column) {                
+	                return false;               
+	        };
+		};
+        table.setRowHeight(30);
+        table.setModel(new DefaultTableModel(moviesWithAccess, headers));
+        table.getColumnModel().getColumn(1).setMinWidth(80);
+        table.getColumnModel().getColumn(1).setMaxWidth(80);
+        scrollPane.setViewportView(table);
+        table.setFocusable(false);
+        
+	}
+	
+	public void setPosterLabel(ImageIcon poster) {
+		posterLabel.setIcon(poster);
+	}
+	
+	public void setTableModel(String [][] moviesWithAccess) {
+		table.setModel(new DefaultTableModel (moviesWithAccess, headers));
+	}
+
+	public void displayMessage(String string) {
+		JOptionPane pane = new JOptionPane();
+		JOptionPane.showMessageDialog(pane, string);	
+	}
+}
+
+	
---
+++ Project/src/Theater/ViewShowtimesGUI.java
@@ -0,0 +1,170 @@
+package Theater;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+
+public class ViewShowtimesGUI extends JFrame{
+	
+	private ManageTheater manageTheater;
+	private JList<ShowTime> showtimesList;
+	private JList<String> showtimesAndMovieList;
+	private JButton confirmShowtimeButton;
+	private JButton viewAllShowtimesButton;
+	private JButton viewSelectedShowtimesButton;
+	private JLabel infoLabel;
+	private JButton returnHomeButton;
+	private JLabel lblNewLabel_1;
+	
+	
+	public ViewShowtimesGUI(boolean userStatus) {
+		
+		getContentPane().setBackground(new Color(176, 196, 222));
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Here are our current showtimes!");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblNewLabel.setBounds(297, 11, 399, 80);
+		getContentPane().add(lblNewLabel);
+		
+		showtimesList = new JList<ShowTime>();
+		showtimesList.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		showtimesList.setBounds(214, 121, 566, 298);
+		getContentPane().add(showtimesList);
+		showtimesList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		showtimesAndMovieList = new JList<String>();
+		showtimesAndMovieList.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		showtimesAndMovieList.setBounds(214, 121, 566, 298);
+		getContentPane().add(showtimesAndMovieList);
+
+		
+		confirmShowtimeButton = new JButton("Confirm Movie Showtime Selection");
+		confirmShowtimeButton.setBounds(528, 441, 252, 50);
+		getContentPane().add(confirmShowtimeButton);
+		
+		viewAllShowtimesButton = new JButton("View All Showtimes");
+		viewAllShowtimesButton.setBounds(418, 525, 160, 44);
+		getContentPane().add(viewAllShowtimesButton);
+		
+		infoLabel = new JLabel("", SwingConstants.CENTER);
+		infoLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		infoLabel.setBounds(357, 88, 289, 22);
+		getContentPane().add(infoLabel);
+		
+		viewSelectedShowtimesButton = new JButton("View Your Selected Movie's Showtimes");
+		viewSelectedShowtimesButton.setBounds(214, 441, 304, 50);
+		getContentPane().add(viewSelectedShowtimesButton);
+		
+		returnHomeButton = new JButton("Return to Home");
+		returnHomeButton.setBounds(10, 11, 135, 35);
+		getContentPane().add(returnHomeButton);
+		
+		lblNewLabel_1 = new JLabel("Login Status: Not Logged In");
+		lblNewLabel_1.setBackground(new Color(255, 222, 173));
+		lblNewLabel_1.setBounds(810, 11, 160, 35);
+		getContentPane().add(lblNewLabel_1);
+		
+		if(userStatus == true) {
+			lblNewLabel_1.setText("Login Status: Logged In");
+        }
+		
+		initializeFrame();
+	}
+	
+	
+	private void initializeFrame() {
+		this.setTitle("View Showtimes");
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setBounds(100, 140, 1000, 650);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+	
+	
+	public void setShowtimesList(DefaultListModel<ShowTime> listModel) {
+		showtimesList.removeAll();
+		showtimesAndMovieList.setVisible(false);
+		showtimesList.setVisible(true);
+		 
+		showtimesList.setModel(listModel);
+	}
+	
+	
+	public void setShowtimesAndMoviesList(DefaultListModel<String> listModel) {
+		showtimesAndMovieList.removeAll();
+		showtimesList.setVisible(false);
+		showtimesAndMovieList.setVisible(true);
+		 
+		showtimesAndMovieList.setModel(listModel);
+	}
+	
+	public JButton getConfirmShowtimeButton() {
+		return confirmShowtimeButton;
+	}
+
+
+	public JButton getViewSelectedShowtimesButton() {
+		return viewSelectedShowtimesButton;
+	}
+	
+	public JButton getViewAllShowtimesButton() {
+		return viewAllShowtimesButton;
+	}
+	
+
+	public void addConfirmShowtimeButtonListener (ActionListener listenForConfirmShowtimeButton) {
+		confirmShowtimeButton.addActionListener(listenForConfirmShowtimeButton);
+	}
+	
+	public void addAllShowtimesButtonListener (ActionListener listenForAllShowtimesButton) {
+		viewAllShowtimesButton.addActionListener(listenForAllShowtimesButton);
+	}
+	
+	public void addSelectedShowtimesButtonListener (ActionListener listenForSelectedShowtimesButton) {
+		viewSelectedShowtimesButton.addActionListener(listenForSelectedShowtimesButton);
+	}
+	
+	
+	public void addReturnListener(ActionListener returnHome) {
+		returnHomeButton.addActionListener(returnHome);
+	}
+	
+	
+	
+	public ManageTheater getManageTheater() {
+		return manageTheater;
+	}
+
+
+	public void setManageTheater(ManageTheater manageTheater) {
+		this.manageTheater = manageTheater;
+	}
+	
+	public ShowTime getShowtimesListSelection() {
+		return showtimesList.getSelectedValue();
+	}
+	
+
+	public void setInfoLabelText(String message) {
+		infoLabel.setText(message);
+	}
+
+
+	public void displayMessage(String string) {
+		JOptionPane pane = new JOptionPane();
+		JOptionPane.showMessageDialog(pane, string);	
+	}
+
+}
