c24b6fbec3d0e75d1415143a650408ebb9a5edf2
santiagofloresm <64857391+santiagofloresm@users.noreply.github.com>
2020-11-30 22:21:27 -0700

    Cleaned manage reservations

--- Project/src/Reservation/ManageReservations.java
+++ Project/src/Reservation/ManageReservations.java
@@ -2,116 +2,61 @@ package Reservation;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-
-import javax.swing.JButton;
 
 public class ManageReservations {
 
     private ReservationSystem reservationSystem;
     private CancellationGUI cancellationGUI;
-    private ReservationGUI reservationGUI;
-    private PurchaseTicketsGUI purchaseTicketsGUI;
+    private ReservationGUI reservationGUI; // ??
+    private PurchaseTicketsGUI purchaseTicketsGUI; // ??
     private Session currentSession;
-    
-    
-    public ManageReservations(ReservationSystem reservationSystem, CancellationGUI cancellationGUI, 
-    		ReservationGUI reservationGUI, PurchaseTicketsGUI purchaseTicketsGUI) {
+
+    public ManageReservations(ReservationSystem reservationSystem, CancellationGUI cancellationGUI,
+                              ReservationGUI reservationGUI, PurchaseTicketsGUI purchaseTicketsGUI) {
         setReservationSystem(reservationSystem);
         this.cancellationGUI = cancellationGUI;
         this.reservationGUI = reservationGUI;
         this.purchaseTicketsGUI = purchaseTicketsGUI;
-        cancellationGUI.addCancellationButtonListener(new CancelReservationButton());
+        cancellationGUI.addButtonActionListener(cancellationGUI.getConfirmCancellationButton(), new CancelReservationButtonListener());
     }
 
-    
-    
-    
-    
-    
-    public String cancelReservation(int reservationId) {
+    class CancelReservationButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            cancel();
+        }
+    }
+
+    private void cancel() {
+        int reservationId = -1;
+        try {
+            reservationId = Integer.parseInt(cancellationGUI.getReservationIdTextField().getText());
+        } catch (NumberFormatException e) {
+            cancellationGUI.displayMessage("Enter a valid reservation ID");
+        }
+        if (reservationId != -1) {
+            String message = cancelReservation(reservationId);
+            cancellationGUI.displayMessage(message);
+        }
+    }
+
+    private String cancelReservation(int reservationId) {
         return reservationSystem.cancelReservation(reservationId);
-    }
-
-    public void displayMovies() {
-        reservationSystem.displayMovies();
-    }
-
-    public void displayVouchers() {
-        reservationSystem.displayVouchers();
-    }
-
-    public void displayReservations() {
-        reservationSystem.displayReservations();
     }
 
     public void setReservationSystem(ReservationSystem reservationSystem) {
         this.reservationSystem = reservationSystem;
     }
 
-    
-    
-    
-    
-    public CancellationGUI getCancellationGUI() {
-		return cancellationGUI;
-	}
+    public ReservationSystem getReservationSystem() {
+        return reservationSystem;
+    }
 
-	public void setCancellationGUI(CancellationGUI cancellationGUI) {
-		this.cancellationGUI = cancellationGUI;
-	}
+    public Session getCurrentSession() {
+        return currentSession;
+    }
 
-	public ReservationGUI getReservationGUI() {
-		return reservationGUI;
-	}
-
-	public void setReservationGUI(ReservationGUI reservationGUI) {
-		this.reservationGUI = reservationGUI;
-	}
-
-	public PurchaseTicketsGUI getPurchaseTicketsGUI() {
-		return purchaseTicketsGUI;
-	}
-
-	public void setPurchaseTicketsGUI(PurchaseTicketsGUI purchaseTicketsGUI) {
-		this.purchaseTicketsGUI = purchaseTicketsGUI;
-	}
-
-	public ReservationSystem getReservationSystem() {
-		return reservationSystem;
-	}
-
-
-
-	public Session getCurrentSession() {
-		return currentSession;
-	}
-
-
-
-
-
-
-	public void setCurrentSession(Session currentSession) {
-		this.currentSession = currentSession;
-	}
-
-
-
-	class CancelReservationButton implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent actionEvent) {
-            int reservationId = 0;
-            try {
-                reservationId = Integer.parseInt(cancellationGUI.getReservationId().getText());
-            } catch (NumberFormatException e) {
-                cancellationGUI.displayMessage("Enter a valid reservation ID");
-            }
-            if (reservationId != 0) {
-                String message = cancelReservation(reservationId);
-                cancellationGUI.displayMessage(message);
-            }
-        }
+    public void setCurrentSession(Session currentSession) {
+        this.currentSession = currentSession;
     }
 }
